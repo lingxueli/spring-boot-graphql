c5ad0da65c3ee3b7317e2360a58159c6f04dc3c8
lingxueli <ll2871@columbia.edu>
2019-10-20 14:33:58 -0600

    add schema

---
+++ src/main/java/com/example/graphql/entity/Dog.java
@@ -0,0 +1,50 @@
+package com.example.graphql.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String breed;
+    private String origin;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setName(String name) {
+        name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+}
