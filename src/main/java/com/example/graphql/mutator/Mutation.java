c18beca5f09532bc2c4693a74d374524102032c7
lingxueli <ll2871@columbia.edu>
2019-10-22 23:01:17 -0600

    add sample data

---
+++ src/main/java/com/example/graphql/mutator/Mutation.java
@@ -0,0 +1,53 @@
+package com.example.graphql.mutator;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.example.graphql.entity.Dog;
+import com.example.graphql.exception.BreedNotFoundException;
+import com.example.graphql.exception.DogNotFoundException;
+import com.example.graphql.repository.DogRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class Mutation implements GraphQLMutationResolver {
+    private DogRepository dogRepository;
+
+    public Mutation(DogRepository dogRepository) {
+        this.dogRepository = dogRepository;
+    }
+
+    public boolean deleteDogBreed(String breed) {
+        boolean deleted = false;
+        Iterable<Dog> allDogs = dogRepository.findAll();
+        // Loop through all dogs to check their breed
+        for (Dog d:allDogs) {
+            if (d.getBreed().equals(breed)) {
+                // Delete if the breed is found
+                dogRepository.delete(d);
+                deleted = true;
+            }
+        }
+        // Throw an exception if the breed doesn't exist
+        if (!deleted) {
+            throw new BreedNotFoundException("Breed Not Found", breed);
+        }
+        return deleted;
+    }
+
+    public Dog updateDogName(String newName, Long id) {
+        Optional<Dog> optionalDog = dogRepository.findById(id);
+
+        if (optionalDog.isPresent()) {
+            Dog dog = optionalDog.get();
+            // Set the new name and save the updated dog
+            dog.setName(newName);
+            dogRepository.save(dog);
+            return dog;
+        } else {
+            throw new DogNotFoundException("Dog Not Found", id);
+        }
+    }
+
+
+}
